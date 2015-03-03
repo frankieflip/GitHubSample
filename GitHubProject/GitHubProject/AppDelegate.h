50e617b4faa79970380c439aa8d422ed60d21542
frankieflip <frank@Franks-Mac.local>
2015-03-03 08:01:49 +0800

    new Xcode project
    new Xcode project commit

---
+++ GitHubProject/GitHubProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitHubProject
+//
+//  Created by Frank on 3/03/2015.
+//  Copyright (c) 2015 Frank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitHubProject/GitHubProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitHubProject
+//
+//  Created by Frank on 3/03/2015.
+//  Copyright (c) 2015 Frank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
